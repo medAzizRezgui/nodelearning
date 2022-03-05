971538a0eafe9efc6f0bb5524744695572221976
Root99 <azizrezgui4@gmail.com>
2022-03-05 23:36:29 +0100

    learning node and express and mongo db

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const cors = require('cors');
-const corsOptions = require('./config/corsOptions');
-const { logger } = require('./middleware/logEvents');
-const errorHandler = require('./middleware/errorHandler');
+const path = require("path");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 3500;
-
+const verifyJWT = require("./middleware/verifyJWT");
+const cookieParser = require("cookie-parser");
 // custom middleware logger
 app.use(logger);
 
@@ -16,25 +17,31 @@ app.use(cors(corsOptions));
 // built-in middleware to handle urlencoded form data
 app.use(express.urlencoded({ extended: false }));
 
-// built-in middleware for json 
+// built-in middleware for json
 app.use(express.json());
-
+// middleware of cookies
+app.use(cookieParser());
 //serve static files
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use("/", express.static(path.join(__dirname, "/public")));
 
 // routes
-app.use('/', require('./routes/root'));
-app.use('/employees', require('./routes/api/employees'));
+app.use("/", require("./routes/root"));
+app.use("/register", require("./routes/register"));
+app.use("/login", require("./routes/login"));
+app.use("/refresh", require("./routes/refresh"));
 
-app.all('*', (req, res) => {
-    res.status(404);
-    if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'));
-    } else if (req.accepts('json')) {
-        res.json({ "error": "404 Not Found" });
-    } else {
-        res.type('txt').send("404 Not Found");
-    }
+app.use(verifyJWT);
+app.use("/employees", require("./routes/employees"));
+// For any other path
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
 });
 
 app.use(errorHandler);

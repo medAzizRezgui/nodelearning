443060eb62bf1413e9243e0940612b8eb225106d
Root99 <azizrezgui4@gmail.com>
2022-03-06 00:56:40 +0100

    added logout controller

---
+++ controllers/logoutController.js
@@ -0,0 +1,44 @@
+const usersDB = {
+  users: require("../model/users.json"),
+  setUsers: function (data) {
+    this.users = data;
+  },
+};
+
+require("dotenv").config();
+const fsPromises = require("fs").promises;
+const path = require("path");
+
+const handleLogout = async (req, res) => {
+  const cookies = req.cookies;
+  if (!cookies?.jwt) return res.sendStatus(204);
+
+  const refreshToken = cookies.jwt;
+
+  // is refresh token in db
+  const foundUser = usersDB.users.find(
+    (person) => person.refreshToken === refreshToken
+  );
+  if (!foundUser) {
+    res.clearCookie("jwt", { httpOnly: true, maxAge: 60 });
+    return res.sendStatus(204);
+  }
+  // delete refresh token
+  const otherUsers = usersDB.users.filter(
+    (person) => person.refreshToken !== foundUser.refreshToken
+  );
+
+  const currentUser = { ...foundUser, refreshToken: "" };
+
+  usersDB.setUsers([...otherUsers, currentUser]);
+  await fsPromises.writeFile(
+    path.join(__dirname, "..", "model", "users.json"),
+    JSON.stringify(usersDB.users)
+  );
+
+  res.clearCookie("jwt", { httpOnly: true, maxAge: 60 });
+
+  res.sendStatus(204);
+};
+
+module.exports = { handleLogout };

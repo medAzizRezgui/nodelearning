afb0410522400dca1cc6769809c3d60c184b5fa6
Root99 <azizrezgui4@gmail.com>
2022-03-06 12:53:56 +0100

    added roles controller and roles middleware

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -21,7 +21,11 @@ const handleNewUser = async (req, res) => {
     //encrypt the password
     const hashedPwd = await bcrypt.hash(pwd, 10);
     //store the new user
-    const newUser = { username: user, password: hashedPwd };
+    const newUser = {
+      username: user,
+      password: hashedPwd,
+      roles: { User: 2001 },
+    };
     usersDB.setUsers([...usersDB.users, newUser]);
     await fsPromises.writeFile(
       path.join(__dirname, "..", "model", "users.json"),
--- routes/employees.js
+++ routes/employees.js
@@ -2,12 +2,21 @@ const express = require("express");
 const router = express.Router();
 const employeesController = require("../controllers/employeesController");
 
+const ROLES_LIST = require("../config/roles_list");
+const VerifyRoles = require("../middleware/verifyRoles");
+
 router
   .route("/")
   .get(employeesController.getAllEmployees)
-  .post(employeesController.createNewEmployee)
-  .put(employeesController.updateEmployee)
-  .delete(employeesController.deleteEmployee);
+  .post(
+    VerifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor),
+    employeesController.createNewEmployee
+  )
+  .put(
+    VerifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor),
+    employeesController.updateEmployee
+  )
+  .delete(VerifyRoles(ROLES_LIST.Admin), employeesController.deleteEmployee);
 
 router.route("/:id").get(employeesController.getEmployee);
 
